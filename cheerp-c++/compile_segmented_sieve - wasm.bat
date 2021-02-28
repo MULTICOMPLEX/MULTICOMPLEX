83e40b147a4a9b0b49e8335d99d21bfe13ca8b4e
MULTICOMPLEX <thepromisedplanet@gmail.com>
2021-02-28 12:01:01 +0100

    update.

--- cheerp-c++/compile_segmented_sieve - wasm.bat
+++
@@ -1,2 +0,0 @@
-C:\cheerp\bin\clang++ -target cheerp-wasm segmented_sieve.cpp -o segmented_sieve_loader.js -O3
-
--- cheerp-c++/derivatives.cpp
+++ cheerp-c++/derivatives.cpp
@@ -4,10 +4,15 @@
 #include <chrono>
 
 #include "/include/multicomplex.hpp"
-
+#include <iomanip>
 
 void webMain()
 {
+	
+	std::cout.setf(std::ios::fixed, std::ios::floatfield);
+	std::cout.precision(12);
+	
+	
 	MX0 x(0.4, -0.5);
 	MX5 y;
 	sh(y, x);
@@ -16,14 +21,12 @@ void webMain()
 	
 	auto d = dv(sin(sqrt(y)));
 	
-	auto end = std::chrono::steady_clock::now();
+	auto end = std::chrono::steady_clock::now(); 
 	
-	std::cout << "Derivatives, using Multicomplex Numbers, WebAssembly" << std::endl; 
-	
-	std::cout << "\nduration d^5/dx^5(sin(sqrt(y))) " <<
-	std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << " uS\n" << std::endl;
-	
-	std::cout << "d^5/dx^5(sin(sqrt(y))), x = 0.4 - 0.5i = -16.4786 - 18.9303 I =  ";	
+	std::cout << "d^5/dx^5(sin(sqrt(x))), x = 0.4 - 0.5i =  ";	
 	std::cout << d << std::endl << std::endl;
+	
+	std::cout << "\nduration d^5/dx^5(sin(sqrt(x))) : " <<
+	std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << " uS\n" << std::endl;
 		
 }
