9100062cfbc308dce2b2918f892bd0f8635d4574
MULTICOMPLEX <thepromisedplanet@gmail.com>
2021-03-10 20:05:38 +0100

    update.

---
+++ web_workers/cheerpWorkerHost.cpp
@@ -0,0 +1,12 @@
+#include <cheerp/client.h>
+#include <cheerp/clientlib.h>
+
+using namespace client;
+
+void webMain()
+{
+        Worker* w = new Worker("cheerpWorker.js");
+        w->addEventListener("message", cheerp::Callback([](MessageEvent* e) {
+                                        console.log((String*)(e->get_data())); }));
+        w->postMessage("Hello World");
+}
